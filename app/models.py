a4feab180f0704743f5bd6d7ba477559b403523f
Code-Lipe <felipe-codelipe@hotmail.com>
2025-08-19 10:36:17 -0300

    feat criar modelo de usuario.

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from app import db
+from datetime import datetime, timezone
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
