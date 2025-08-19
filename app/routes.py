bcfeaf0b90842782dedbfad1f3d209f22d3ee545
Code-Lipe <felipe-codelipe@hotmail.com>
2025-08-19 11:01:51 -0300

    feat: criar rota de registro de usuário

---
+++ app/routes.py
@@ -0,0 +1,29 @@
+from flask import request, jsonify
+from app import app, db
+from app.models import User
+from app.services import hash_password
+
+@app.route('/registrar', methods=['POST'])
+def register():
+    data = request.get_json()
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
+
+    if not username or not email or not password:
+        return jsonify({'message': 'Dados de usuário incompletos'}), 400
+
+    hashed_password = hash_password(password)
+
+    new_user = User(
+        username=username,
+        email=email,
+        password=hashed_password
+    )
+
+    try:
+        db.session.add(new_user)
+        db.session.commit
+        return jsonify({'message': 'Usuário registrado com sucesso'}), 201
+    except:
+        return jsonify({'message': 'Erro ao registrar usuário. Verifique se o nome de usuário ou e-mail já existem.'}), 409
