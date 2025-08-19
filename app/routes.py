0f45df7d10ca5a2e465dca70c8ebe50a754cb0f6
Code-Lipe <felipe-codelipe@hotmail.com>
2025-08-19 17:41:10 -0300

    feat: usar id do usuario como identidade do JWT para maior seguranca

--- app/routes.py
+++ app/routes.py
@@ -46,5 +46,5 @@ def login():
 @app.route('/protected',  methods=['GET'])
 @jwt_required()
 def protected():
-    current_user = get_jwt_identity()
-    return jsonify(logged_in_as=current_user), 200
+    current_user_id = get_jwt_identity()
+    return jsonify(logged_in_as=current_user_id), 200
